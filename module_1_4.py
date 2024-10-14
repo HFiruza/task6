5ac6a47b6ec2fbafb100eb12e173ccd4c83957eb
Фируза <ferika2525@gmail.com>
2024-10-14 14:19:41 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input('Введите ваше имя: ')
+#print('Здравствуйте,', my_string)
+#current_year = 2024
+#date_of_birth = input('В каком году Вы родились? ')
+#age = current_year -int(date_of_birth)
+#print('В этом году вам', age, 'года')
+
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
